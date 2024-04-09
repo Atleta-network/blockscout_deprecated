817b3eaca0609ae3b7ce7dfa3f51f8f5ad41793f
r8bywork <vanya.smolensky@gmail.com>
2024-04-09 12:30:32 +0300

    Fix CORS issue in buildUrl.ts && buildApiUrl.ts for pw tests && update screens

--- playwright/utils/buildApiUrl.ts
+++ playwright/utils/buildApiUrl.ts
@@ -1,11 +1,18 @@
 import { compile } from 'path-to-regexp';
 
-import type { ResourceName, ResourcePathParams } from 'lib/api/resources';
+import config from 'configs/app';
+import isNeedProxy from 'lib/api/isNeedProxy';
 import { RESOURCES } from 'lib/api/resources';
+import type { ApiResource, ResourceName, ResourcePathParams } from 'lib/api/resources';
 
-export default function buildApiUrl<R extends ResourceName>(resourceName: R, pathParams?: ResourcePathParams<R>) {
-  const resource = RESOURCES[resourceName];
-  const defaultApi = 'http://' + process.env.NEXT_PUBLIC_API_HOST;
-  const origin = 'endpoint' in resource && resource.endpoint ? resource.endpoint + (resource.basePath ?? '') : defaultApi;
-  return origin + compile(resource.path)(pathParams);
+export default function buildUrl<R extends ResourceName>(
+  resourceName: R,
+  pathParams?: ResourcePathParams<R>,
+): string {
+  const resource: ApiResource = RESOURCES[resourceName];
+  const baseUrl = isNeedProxy() ? config.app.baseUrl : (resource.endpoint || config.api.endpoint);
+  const basePath = resource.basePath !== undefined ? resource.basePath : config.api.basePath;
+  const path = isNeedProxy() ? '/node-api/proxy' + basePath + resource.path : basePath + resource.path;
+  const url = new URL(compile(path)(pathParams || {}), baseUrl);
+  return url.toString();
 }
